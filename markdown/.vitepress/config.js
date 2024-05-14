dd85d6b4d882b0d5498a03ec2a4439b64a5abff3
Richard Wang <wangleihd@gmail.com>
2024-05-14 13:07:42 +0800

    Init commit.

---
+++ markdown/.vitepress/config.js
@@ -0,0 +1,146 @@
+import { withMermaid } from "vitepress-plugin-mermaid";
+import mathjax3 from 'markdown-it-mathjax3';
+
+const customElements = [
+  'mjx-container',
+  'mjx-assistive-mml',
+  'math',
+  'maction',
+  'maligngroup',
+  'malignmark',
+  'menclose',
+  'merror',
+  'mfenced',
+  'mfrac',
+  'mi',
+  'mlongdiv',
+  'mmultiscripts',
+  'mn',
+  'mo',
+  'mover',
+  'mpadded',
+  'mphantom',
+  'mroot',
+  'mrow',
+  'ms',
+  'mscarries',
+  'mscarry',
+  'mscarries',
+  'msgroup',
+  'mstack',
+  'mlongdiv',
+  'msline',
+  'mstack',
+  'mspace',
+  'msqrt',
+  'msrow',
+  'mstack',
+  'mstack',
+  'mstyle',
+  'msub',
+  'msup',
+  'msubsup',
+  'mtable',
+  'mtd',
+  'mtext',
+  'mtr',
+  'munder',
+  'munderover',
+  'semantics',
+  'math',
+  'mi',
+  'mn',
+  'mo',
+  'ms',
+  'mspace',
+  'mtext',
+  'menclose',
+  'merror',
+  'mfenced',
+  'mfrac',
+  'mpadded',
+  'mphantom',
+  'mroot',
+  'mrow',
+  'msqrt',
+  'mstyle',
+  'mmultiscripts',
+  'mover',
+  'mprescripts',
+  'msub',
+  'msubsup',
+  'msup',
+  'munder',
+  'munderover',
+  'none',
+  'maligngroup',
+  'malignmark',
+  'mtable',
+  'mtd',
+  'mtr',
+  'mlongdiv',
+  'mscarries',
+  'mscarry',
+  'msgroup',
+  'msline',
+  'msrow',
+  'mstack',
+  'maction',
+  'semantics',
+  'annotation',
+  'annotation-xml',
+];
+
+export default withMermaid({
+  // your existing vitepress config...
+  title: 'FTA nails',
+  description: 'FTA Nails 工作文档',
+	lastUpdated: true,
+	markdown: {
+    theme: {
+      light: 'github-light',
+      dark: 'github-light',
+    },
+		lineNumbers: true,
+    config: (md) => {
+      md.use(mathjax3);
+    },
+	},
+	themeConfig: {
+		sidebar: [
+      {
+        text: '工作文档',
+        collapsible: true,
+        collapsed: true,
+        items: [
+          { text: '工作模块', link: '/nails/plan' },
+          { text: '工作平台', link: '/nails/shop' },
+          { text: '常用工具', link: '/nails/tools' },
+        ]
+      },
+    ],
+		
+		footer: {
+			license: {
+				text: 'MIT License',
+				link: 'https://opensource.org/licenses/MIT'
+			},
+			copyright: `Copyright © 2022-${new Date().getFullYear()} Richard Wang`
+		},
+		lastUpdatedText: 'Updated Date',
+		docFooter: {
+      prev: 'Pagina prior',
+      next: 'Proxima pagina'
+    }
+	},
+  mermaid: {
+
+  },
+  vue: {
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => customElements.includes(tag),
+      },
+    },
+  },
+});
---
+++ markdown/index.md
@@ -0,0 +1,24 @@
+---
+layout: home
+
+title: FTA Nails 
+titleTemplate: 
+
+hero:
+  name: FTA Nails
+  text: 电商平台工作文档
+  tagline: 
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /nails/tools
+    # - theme: alt
+    #   text: View on GitHub
+    #   link: https://github.com/vuejs/vitepress
+
+features:
+  - title:  常用工具
+    details: 常用工具
+    link: /nails/tools
+
+---
